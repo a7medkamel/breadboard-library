7a5a5d8d66b84fe791324ff38a9bae181eabdc2f
Ahmed Kamel <ak@ahmedkamel.net>
2017-03-09 04:26:53 -0800

    exit phantom js on end

--- http/to_png.js
+++ http/to_png.js
@@ -17,15 +17,13 @@ var phantom = require('phantom')
   ;
 
 module.exports = function(req, res, next) {
-  phantom
-    .create()
-    .tap((ph) => {
-      return ph
-              .createPage()
-              .then((page) => {
-                return page
-                        .property('viewportSize', { width: 1920, height: 1080 })
-                        .then(() => page);
+  Promise
+    .resolve(phantom.create())
+    .then((ph) => {
+      return Promise
+              .resolve(ph.createPage())
+              .tap((page) => {
+                return page.property('viewportSize', { width: 1920, height: 1080 });
               })
               .then((page) => {
                 var url = req.body;
@@ -47,14 +45,10 @@ module.exports = function(req, res, next) {
 
                           res.end(new Buffer(base64, 'base64'), 'binary');
                         });
+              })
+              .finally(() => {
+                return ph.kill();
               });
     })
-    .finally(() => {
-      let exec = require('child_process').exec
-    
-      return ph
-              .kill()
-              .then(() => ph.exit())
-              .then(() => Promise.fromCallback((cb) => exec(`kill ${ph.process.pid}`, cb)));
-    });
+    .catch((err) => next);
 };
